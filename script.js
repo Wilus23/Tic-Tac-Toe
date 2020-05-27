833b4146b572bff2cf4edcbf608ed2fb82ac4dd1
wilus23 <w.lepkowski23@gmail.com>
2020-05-27 15:52:03 +0200

    1. After clicking on area, the icons are added. 2. Cursor change (from League of Legends) 3. Grid repair

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 console.log('Connected!');
 
-let area = document.querySelector('div.block')
-area.addEventListener('click', function(){
-    console.log('click')
+const PLAYER1 = "fa-circle-o";
+const PLAYER2 = "fa-times";
+let round = 1;
+
+
+
+let area = [...document.querySelectorAll('.block')];
+
+function pick(event){
+    const turn = round % 2 === 0 ? PLAYER2 : PLAYER1;
+    event.target.classList.add(turn);
+    round++;
+}
+area.forEach(block=> block.addEventListener('click', pick));
+
+
+//reset
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+    location.reload();
 })
